7fda44b4d877e3cc9d8b2573c64bbac414d29611
Christopher Hampel <christopher.hampel@yaho.de>
2024-03-05 20:48:26 +0100

    pokemon card

--- script.js
+++ script.js
@@ -7,20 +7,33 @@ async function loadPokemon(){
     let response = await fetch(url);
     let responseAsJson = await response.json();
     let pokemonName = responseAsJson['name'];
-    let pokemonImage = responseAsJson['sprites']['front_shiny'];   
-    
-    showCurrentPokemon(pokemonName, pokemonImage);
-
+    // let pokemonType = responseAsJson['types']['0']['type']['name'];
+    let pokemonImage = responseAsJson['sprites']['front_shiny'];  
+    let pokemonCard = document.getElementById('pokemonCard');
+    pokemonCard.innerHTML = '';
+    // let currentImage = document.getElementById('pokemonImage');
+    // currentImage.innerHTML = `<img src=\"${pokemonImage}">`;
+    pokemonCard.innerHTML += returnHtmlPokemonCard(responseAsJson, pokemonImage);
     console.log(responseAsJson);
 }
 
-function showCurrentPokemon(pokemonName, pokemonImage) {
-    let currentName = document.getElementById('pokemonName');
-    let currentImage = document.getElementById('pokemonImage');
-    
-    currentName.innerHTML = '';
-    currentName.innerHTML = `${pokemonName}`
-
-    currentImage.innerHTML = '';
-    currentImage.innerHTML = `<img src=\"${pokemonImage}">`;
+function returnHtmlPokemonCard(currentPokemon, pokemonImage) {
+    return `
+    <div class="background">
+    <div class="pokemon-card">
+        <div class="flex-column pd-16">
+            <h3>${currentPokemon['name']}</h3>
+            <div class="flex-column">
+                <span>${currentPokemon['types']['0']['type']['name']}</span>
+                <span>Text</span>
+            </div>
+        </div>
+        <div class="pokemon-card-rightside">
+            <div class="pd-8">#${currentPokemon['id']}</div>
+            <div class="pokemon-main-image" id="pokemonImage">
+                <img src="${pokemonImage}">
+            </div>
+        </div>
+    </div>
+</div>`
 }
